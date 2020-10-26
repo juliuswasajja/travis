cb1a15e6e02ed6cc3f03a3e30ab7d7649e08d9b8
juliuswasajja-dig <65942820+juliuswasajja-dig@users.noreply.github.com>
2020-10-26 20:23:38 +0300

    added test files

---
+++ test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const fibonacci = require('./fabonacci2');
+
+describe ('fibonacci', ()=>{
+    
+    it('Index 10 is 89', () =>{
+        assert.equal(89, fibonacci(10))   
+    })
+    it('Index 20 is 10946', () =>{
+        assert.equal(10946, fibonacci(20))  
+    })
+    it('Index 4 is 5', () =>{
+        assert.equal(5, fibonacci(4))  
+    })
+    it('Index 1 is 1', () =>{
+        assert.equal(1, fibonacci(1))  
+    })
+})
